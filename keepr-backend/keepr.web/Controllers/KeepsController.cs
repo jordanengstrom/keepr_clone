759f922fb92f883a028e7b56f993e708f7bb20d7
jordanengstrom <jordan.engstrom@gmail.com>
2021-01-28 17:00:01 -0700

    first commit

---
+++ keepr-backend/keepr.web/Controllers/KeepsController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace keepr.web.Controllers
+{
+    public class KeepsController : Controller
+    {
+        // GET: Keeps
+        public ActionResult Index()
+        {
+
+            return View();
+        }
+    }
+}
